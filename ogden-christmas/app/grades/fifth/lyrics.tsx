8cce76964b81c9a1a20a4d1ed42e5f05cb7f5d79
Mark DeKraker <mrdekraker@Marks-Laptop.local>
2023-11-05 12:30:52 -0600

    finished 5th grade

---
+++ ogden-christmas/app/grades/fifth/lyrics.tsx
@@ -0,0 +1,84 @@
+import React from 'react';
+
+const Lyrics: React.FC = () => {
+  return (
+    <div className="flex flex-col items-center justify-center text-center mx-auto">
+      <p className="text-lg m-1">
+        I don't want a lot for Christmas <br />
+        There is just one thing I need <br />
+        I don't care about the presents <br />
+        Underneath the Christmas tree <br />
+        I just want you for my own <br />
+        More than you could ever know <br />
+        Make my wish come true <br />
+        All I want for Christmas <br />
+        Is you <br />
+      </p>
+
+      <p className="text-lg m-1">
+        I don't want a lot for Christmas <br />
+        There is just one thing I need (And I)<br />
+        I don't care about the presents <br />
+        Underneath the Christmas tree <br />
+        I don't need to hang my stocking <br />
+        There upon the fireplace (Ahhh) <br />
+        Santa Claus won't make me happy <br />
+        With a toy on Christmas day <br />
+      </p>
+
+      <p className="text-lg m-1">
+        I just want you for my own <br />
+        More than you could ever know <br />
+        Make my wish come true <br />
+        All I want for Christmas is you <br />
+        You baby <br />
+      </p>
+
+      <p className="text-lg m-1">
+        I won't ask for much this Christmas <br />
+        I won't even wish for snow <br />
+        I'm just gonna keep on waiting <br />
+        Underneath the mistletoe <br />
+        I won't make a list and send it <br />
+        To the North Pole for Saint Nick <br />
+        I won't even stay awake to <br />
+        Hear those magic reindeer click <br />
+      </p>
+
+      <p className="text-lg m-1">
+        'Cause I just want you here tonight <br />
+        Holding on to me so tight <br />
+        What more can I do <br />
+        Oh, Baby all I want for Christmas is you <br />
+        You, baby <br />
+      </p>
+
+      <p className="text-lg m-1">
+        Oh, all the lights are shining <br />
+        So brightly everywhere <br />
+        And the sound of children's <br />
+        Laughter fills the air <br />
+        And everyone is singing <br />
+        I hear those sleigh bells ringing <br />
+        Santa won't you bring me <br />
+        The one I really need <br />
+        Won't you please bring my baby to me quickly <br />
+      </p>
+
+      <p className="text-lg m-1">
+        I don't want a lot for Christmas <br />
+        This is all I'm asking for <br />
+        I just wanna see my baby <br />
+        Standing right outside my door <br />
+        I just want you for my own <br />
+        More than you could ever know <br />
+        Make my wish come true <br />
+        Baby all I want for Christmas is <br />
+        You <br />
+      </p>
+    </div>
+
+  );
+};
+
+export default Lyrics;
--- ogden-christmas/app/grades/fifth/page.tsx
+++ ogden-christmas/app/grades/fifth/page.tsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import Lyrics from './lyrics'
 
-const fifthGrade = () => {
+type Props = {}
+
+const FifthGrade = (props: Props) => {
   return (
-    <div>fifthGrade</div>
+    <main className="flex flex-col items-center justify-center px-4 md:px-0 w-full flex-grow">
+      <h1 className="text-4xl md:text-7xl font-bold text-slate-200 text-center mb-6">All I Want For Christmas</h1>
+      <div className="flex flex-col justify-center items-center overflow-auto w-full">
+        <div className="lyrics w-full md:w-1/2 flex flex-col items-center mb-4 md:mb-2 overflow-auto">
+          <div className="w-full flex flex-col md:flex-row">
+            <iframe
+              className="my-2 px-1 md:h-[200px] md:w-1/2"
+              width="100%"
+              height="200"
+              src="https://www.youtube.com/embed/RmUWWVZw28E?si=xHOj-mFoW6SWIjn3"
+              title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen />
+            <iframe
+              className="my-2 px-1 md:h-[200px] md:w-1/2"
+              width="100%" height="200"
+              src="https://www.youtube.com/embed/l3iEnBThkIc?si=aECf2Mp63diemxcY&amp;start=11"
+              title="YouTube video player"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen />
+          </div>
+          <div className="h-[40vh] w-full">
+            <Lyrics />
+          </div>
+        </div>
+      </div>
+    </main>
   )
 }
 
-export default fifthGrade
+export default FifthGrade
