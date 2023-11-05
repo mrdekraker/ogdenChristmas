df37e577887634fee10a8f8c3c91eb3e7724070e
Mark DeKraker <mrdekraker@Marks-Laptop.local>
2023-11-05 16:18:13 -0600

    edited navbar

--- ogden-christmas/app/components/navbar.tsx
+++ ogden-christmas/app/components/navbar.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import Link from 'next/link'
-import Image from 'next/image'
+'use client' 
 
-type Props = {}
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import { usePathname } from 'next/navigation';
+
+type Props = {};
 
 const Navbar = (props: Props) => {
+  const pathname = usePathname();
 
   const christmasColors = () => {
     const text = "OGDEN CHRISTMAS POPTACULAR!";
@@ -14,34 +18,40 @@ const Navbar = (props: Props) => {
     });
   };
 
+  const isLinkActive = (path: string) => {
+    return pathname === path ? 'active' : '';
+  };
+
   return (
-    <header className="border-b-2 mb-2">
-      <nav className="flex justify-center items-center w-full text-slate-100 px-2 md:px-0">
+    <header className="mb-5 border">
+      <nav className="flex justify-center items-center w-full text-slate-100">
         <div className="hidden sm:block">
           <Link href="/">
-            <Image
-              src="/ogden-logo.png"
-              alt="Ogden Logo"
-              width={360}
-              height={360}
-              style={{
-                width: '100%',
-                height: 'auto',
-              }}
-            />
+            <Image src="/ogden-logo.png" alt="Ogden Logo" width={360} height={360} />
           </Link>
         </div>
-        <div className="flex items-center gap-2 md:gap-10 text-xl md:text-2xl mt-2" style={{ transform: 'translateY(-7px)' }}>
-          <Link href="/grades/first" className="hover:text-slate-200">First</Link>
-          <Link href="/grades/second" className="hover:text-slate-200">Second</Link>
-          <Link href="/grades/third" className="hover:text-slate-200">Third</Link>
-          <Link href="/grades/fourth" className="hover:text-slate-200">Fourth</Link>
-          <Link href="/grades/fifth" className="hover:text-slate-200">Fifth</Link>
-          <Link href="/" className="hover:text-slate-200">Home</Link>
+        <div className="flex items-center gap-10 text-2xl mt-2" style={{ transform: 'translateY(-7px)' }}>
+          <Link passHref href="/grades/first" className="hover:text-slate-200">
+            <div className={`link ${isLinkActive('/grades/first')}`}>First</div>
+          </Link>
+          <Link passHref href="/grades/second" className="hover:text-slate-200">
+            <div className={`link ${isLinkActive('/grades/second')}`}>Second</div>
+          </Link>
+          <Link passHref href="/grades/third" className="hover:text-slate-200">
+            <div className={`link ${isLinkActive('/grades/third')}`}>Third</div>
+          </Link>
+          <Link passHref href="/grades/fourth" className="hover:text-slate-200">
+            <div className={`link ${isLinkActive('/grades/fourth')}`}>Fourth</div>
+          </Link>
+          <Link passHref href="/grades/fifth" className="hover:text-slate-200">
+            <div className={`link ${isLinkActive('/grades/fifth')}`}>Fifth</div>
+          </Link>
+          <Link passHref href="/" className="hover:text-slate-200">
+            <div className={`link ${isLinkActive('/')}`}>Home</div>
+          </Link>
         </div>
       </nav>
-
-      <div className="flex justify-center gap-8 mb-4 mx-2 md:mx-10 text-center text-slate-100">
+      <div className="flex justify-center gap-8 mb-4 mx-10 text-center text-slate-100">
         <div className="basis-2/3 md:mt-3">
           <div className="flex flex-col">
             <h1 className="font-bold text-3xl md:text-5xl">Ogden Presents:</h1>
@@ -50,7 +60,7 @@ const Navbar = (props: Props) => {
         </div>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
