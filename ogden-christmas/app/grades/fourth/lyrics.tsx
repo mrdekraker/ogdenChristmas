8bec602405232d85eab23720a0681acc9b540fb4
Mark DeKraker <mrdekraker@Marks-Laptop.local>
2023-11-05 12:19:54 -0600

    finished 4th grade

---
+++ ogden-christmas/app/grades/fourth/lyrics.tsx
@@ -0,0 +1,162 @@
+import React from 'react';
+
+const Lyrics: React.FC = () => {
+  return (
+    <div className="flex flex-col items-center justify-center text-center mx-auto">
+      <p className="text-lg m-1">
+        On the first day of Christmas <br />
+        My true love gave to me <br />
+        A partridge in a pear tree <br />
+      </p>
+
+      <p className="text-lg m-1">
+        On the second day of Christmas <br />
+        My true love gave to me <br />
+        Two turtle doves <br />
+        And a partridge in a pear tree <br />
+      </p>
+
+      <p className="text-lg m-1">
+        On the third day of Christmas <br />
+        My true love gave to me <br />
+        Three French hens <br />
+        Two turtle doves <br />
+        And a partridge in a pear tree <br />
+      </p>
+
+      <p className="text-lg m-1">
+        On the fourth day of Christmas <br />
+        My true love gave to me <br />
+        Four calling birds <br />
+        Three French hens <br />
+        Two turtle doves <br />
+        And a partridge in a pear tree <br />
+      </p>
+
+      <p className="text-lg m-1">
+        On the fifth day of Christmas <br />
+        My true love gave to me <br />
+        Five golden rings <br />
+        Four calling birds <br />
+        Three French hens <br />
+        Two turtle doves <br />
+        And a partridge in a pear tree <br />
+      </p>
+
+      <p className="text-lg m-1">
+        What's a partridge? <br />
+        What's a pear tree? <br />
+        I don't know so please don't ask me <br />
+        But I can bet those are terrible gifts to get<br />
+      </p>
+
+      <p className="text-lg m-1">
+        On the sixth day of Christmas <br />
+        My true love gave to me <br />
+        Six geese a laying <br />
+        Five golden rings <br />
+        Four calling birds <br />
+        Three French hens <br />
+        Two turtle doves <br />
+        And a partridge in a pear tree <br />
+      </p>
+
+      <p className="text-lg m-1">
+        On the seventh day of Christmas <br />
+        My true love gave to me <br />
+        Seven swans a swimming <br />
+        Six geese a laying <br />
+        Five golden rings <br />
+        Four calling birds <br />
+        Three French hens <br />
+        Two turtle doves <br />
+        And a partridge in a pear tree <br />
+      </p>
+
+      <p className="text-lg m-1">
+        On the eighth day of Christmas <br />
+        My true love gave to me <br />
+        Eight maids a milking <br />
+        Seven swans a swimming <br />
+        Six geese a laying <br />
+        Five golden rings <br />
+        Four calling birds <br />
+        Three French hens <br />
+        Two turtle doves <br />
+        And a partridge in a pear tree <br />
+      </p>
+
+      <p className="text-lg m-1">
+        On the ninth day of Christmas <br />
+        My true love gave to me <br />
+        Nine ladies dancing <br />
+        Eight maids a milking <br />
+        Seven swans a swimming <br />
+        Six geese a laying <br />
+        Oooh, Eee Ooooh Ahhhh <br />
+        Four calling birds <br />
+        Three French hens <br />
+        Two turtle doves <br />
+        And a partridge in a pear tree <br />
+      </p>
+
+      <p className="text-lg m-1">
+        What's a partridge? <br />
+        What's a pear tree? <br />
+        I don't know so please don't ask me <br />
+        But I can bet those are terrible gifts to get<br />
+      </p>
+
+      <p className="text-lg m-1">
+        On the tenth day of Christmas <br />
+        My true love gave to me <br />
+        Ten lords a leaping <br />
+        Nine ladies dancing <br />
+        Eight maids a milking <br />
+        Seven swans a swimming <br />
+        Six geese a laying <br />
+        Five pinball machines <br />
+        Four ... <br />
+        Three French <br />
+        Two .. doves <br />
+        And a partridge in a pear tree <br />
+      </p>
+
+      <p className="text-lg m-1">
+        On the eleventh day of Christmas <br />
+        My true love gave to me <br />
+        Eleven pipers piping <br />
+        Ten lords a leaping <br />
+        Nine ladies dancing <br />
+        Eight maids a milking <br />
+        Seven swans a swimming <br />
+        Six geese a laying <br />
+        Five golden rings <br />
+        Four calling birds <br />
+        Three French hens <br />
+        Two turtle doves <br />
+        And a partridge in a pear tree <br />
+      </p>
+
+      <p className="text-lg m-1">
+        On the twelfth day of Christmas <br />
+        My true love gave to me <br />
+        Twelve drummers drumming <br />
+        Eleven pipers piping <br />
+        Ten lords a leaping <br />
+        Nine ladies dancing <br />
+        Eight maids a milking <br />
+        Seven swans a swimming <br />
+        Six geese a laying <br />
+        Five golden rings <br />
+        Four calling birds <br />
+        Three French hens <br />
+        Two turtle doves <br />
+        And a partridge in a pear tree <br />
+      </p>
+    </div>
+
+  );
+};
+
+export default Lyrics;
--- ogden-christmas/app/grades/fourth/page.tsx
+++ ogden-christmas/app/grades/fourth/page.tsx
@@ -1,11 +1,39 @@
 import React from 'react'
+import Lyrics from './lyrics'
 
 type Props = {}
 
-const fourthGrade = (props: Props) => {
+const ThirdGrade = (props: Props) => {
   return (
-    <div>fourthGrade</div>
+    <main className="flex flex-col items-center justify-center px-4 md:px-0 w-full flex-grow">
+      <h1 className="text-4xl md:text-7xl font-bold text-slate-200 text-center mb-6">12 Days of Christmas!</h1>
+      <p>On the Karaoke version of this song: Listen for the drum sticks, then come in. The count is going to be 1 - 2 - 1 2 3 4</p>
+      <div className="flex flex-col justify-center items-center overflow-auto w-full">
+        <div className="lyrics w-full md:w-1/2 flex flex-col items-center mb-4 md:mb-2 overflow-auto">
+          <div className="w-full flex flex-col md:flex-row">
+            <iframe
+              className="my-2 px-1 md:h-[200px] md:w-1/2"
+              width="100%"
+              height="200"
+              src="https://www.youtube.com/embed/5FWRyh4GSB8?si=_bPvUGODAH7nVx5-"
+              title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen />
+            <iframe 
+              className="my-2 px-1 md:h-[200px] md:w-1/2"
+              width="100%" 
+              height="200" 
+              src="https://www.youtube.com/embed/ez09JrIGy6g?si=i3LPzgH-0O-CFkMc" 
+              title="YouTube video player" 
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+              allowFullScreen>
+            </iframe>
+          </div>
+          <div className="h-[40vh] w-full">
+            <Lyrics />
+          </div>
+        </div>
+      </div>
+    </main>
   )
 }
 
-export default fourthGrade
+export default ThirdGrade
